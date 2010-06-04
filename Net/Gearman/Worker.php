f42885f19f1f5b4011c3dccb9de376bd45342263
Bernhard K. Weisshuhn <bkw@weisshuhn.de>
2010-06-04 15:18:55 +0800

    use initParams everywhere

--- Net/Gearman/Worker.php
+++ Net/Gearman/Worker.php
@@ -85,6 +85,12 @@ class Net_Gearman_Worker
      */
     protected $abilities = array();
 
+    /**
+     * Parameters for job contructors, indexed by ability name
+     *
+     * @var array $initParams
+     */
+    protected $initParams = array();
     
     /**
      * Callbacks registered for this worker
@@ -169,11 +175,12 @@ class Net_Gearman_Worker
      *
      * @param string  $ability Name of functcion/ability
      * @param integer $timeout How long to give this job
+     * @param array $initParams Parameters for job constructor
      *
      * @return void
      * @see Net_Gearman_Connection::send()
      */
-    public function addAbility($ability, $timeout = null)
+    public function addAbility($ability, $timeout = null, $initParams=array())
     {
         $call   = 'can_do';
         $params = array('func' => $ability);
@@ -181,6 +188,8 @@ class Net_Gearman_Worker
             $params['timeout'] = $timeout;
             $call              = 'can_do_timeout';
         }
+
+        $this->initParams[$ability] = $initParams;
         
         $this->abilities[$ability] = $timeout;
         
@@ -268,7 +277,9 @@ class Net_Gearman_Worker
             if ($retryChange === true) {
                 // broadcast all abilities to all servers
                 foreach ($this->abilities as $ability => $timeout) {
-                    $this->addAbility($ability, $timeout);
+                    $this->addAbility(
+                        $ability, $timeout, $this->initParams[$ability]
+                    );
                 }
             }
 
@@ -320,7 +331,9 @@ class Net_Gearman_Worker
             }
         }
 
-        $job = Net_Gearman_Job::factory($name, $socket, $handle);
+        $job = Net_Gearman_Job::factory(
+            $name, $socket, $handle, $this->initParams[$name]
+        );
         try {
             $this->start($handle, $name, $arg);
             $res = $job->run($arg); 
